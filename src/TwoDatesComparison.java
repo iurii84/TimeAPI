0a8986d13edcec9881aa57a0adf03f712a2beb49
Iuri Ostrikov <iurii84@gmail.com>
2019-06-04 20:51:08 -0400

    Two Dates Comparison

--- src/TwoDatesComparison.java
+++ src/TwoDatesComparison.java
@@ -1,6 +1,6 @@
 import java.time.LocalDate;
 
-public class ThoDatesComparison {
+public class TwoDatesComparison {
     public static void main(String[] args) {
         LocalDate now = LocalDate.now();
         LocalDate future = LocalDate.of(2025, 5, 2);
